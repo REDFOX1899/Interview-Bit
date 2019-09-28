c47dbba545cf19c496972c6cfa8a0a3df86a06fb
Shantanu Patil <shantanupatil1899@gmail.com>
2019-09-28 19:26:04 +0530

    Update Largest_Number.cpp

--- Arrays/Largest_Number.cpp
+++ Arrays/Largest_Number.cpp
@@ -1,3 +1,13 @@
+/*
+Given a list of non negative integers, arrange them such that they form the largest number.
+
+For example:
+
+Given [3, 30, 34, 5, 9], the largest formed number is 9534330.
+
+Note: The result may be very large, so you need to return a string instead of an integer.
+*/
+
 bool checkLarger(int a, int b){
     string aa = to_string(a);
     string bb = to_string(b);
