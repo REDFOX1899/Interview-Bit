14238a85365657b88d5aa01b17488d8c24b7c7d4
Shantanu Patil <shantanupatil1899@gmail.com>
2019-09-29 00:12:35 +0530

    Create GreatestCommonDivisor.cpp

---
+++ Math/GreatestCommonDivisor.cpp
@@ -0,0 +1,40 @@
+/*
+In mathematics, the greatest common divisor (gcd) of two or more integers, which are not all zero,
+is the largest positive integer that divides each of the integers. For example, the gcd of 8 and 12 is 4.
+
+Given 2 non negative integers m and n, find gcd(m, n)
+
+GCD of 2 integers m and n is defined as the greatest integer g such that g is a divisor of both m and n.
+Both m and n fit in a 32 bit signed integer
+
+my approach 
+
+int Solution::gcd(int A, int B) {
+    int min;
+    if(A<B)
+        min = A;
+    else 
+        min = B;
+        
+    if(min ==0)
+        return (A+B);
+    
+    for(int i =min; i>0; i--){
+        if(A%i ==0 && B%i ==0)
+            return i;
+    }
+    
+    return 1;
+        
+}
+*/
+
+int Solution::gcd(int A, int B) {
+    while(A != 0){
+        int temp = B;
+        B = A;
+        A = temp%A;
+    }
+    
+    return B;
+}
