ba21ea4e08e86aa0e94136a7c2fe55f517bb1170
Shantanu Patil <shantanupatil1899@gmail.com>
2019-09-28 19:28:04 +0530

    Update Max-Non-Negative-SubArray.cpp

--- Arrays/Max-Non-Negative-SubArray.cpp
+++ Arrays/Max-Non-Negative-SubArray.cpp
@@ -1,3 +1,16 @@
+/*
+Given an array of integers, A of length N, find out the maximum sum sub-array of non negative numbers from A.
+
+The sub-array should be contiguous i.e., a sub-array created by choosing the second and fourth element and skipping the third element is invalid.
+
+Maximum sub-array is defined in terms of the sum of the elements in the sub-array.
+
+Find and return the required subarray.
+
+NOTE : 1. If there is a tie, then compare with segment's length and return segment which has maximum length.
+       2. If there is still a tie, then return the segment with minimum starting index.
+*/
+
 vector<int> Solution::maxset(vector<int> &A) {
    long long int start=0, end=0, ansStart=0, ansLength=0, ansEnd=-1, sumTillNow = INT_MIN, maxTillNow=INT_MIN;
    int i=0;
