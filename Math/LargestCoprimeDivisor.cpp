38eb6c317a8b0351e336c2b987a83a8f2cf96895
Shantanu Patil <shantanupatil1899@gmail.com>
2019-09-30 18:52:16 +0530

    Update LargestCoprimeDivisor.cpp

--- Math/LargestCoprimeDivisor.cpp
+++ Math/LargestCoprimeDivisor.cpp
@@ -9,6 +9,25 @@ A = 30
 B = 12
 We return
 X = 5
+
+Input : x = 15
+        y = 3 
+Output : a = 5
+Explanation: 5 is the max integer 
+which satisfies both the conditions.
+             15 % 5 =0
+             gcd(5, 3) = 1
+Hence, output is 5. 
+
+Input : x = 14
+        y = 28
+Output : a = 1
+Explanation: 14 % 1 =0
+             gcd(1, 28) = 1
+Hence, output is 1. 
+
+ Here, first we will remove the common factors of x and y from x by finding 
+ the greatest common divisor (gcd) of x and y and dividing x with that gcd.
 */
 
 int Solution::cpFact(int A, int B) {
