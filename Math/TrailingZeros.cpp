3a16babe96ccb1671b930b39ef6067438568ecc8
Shantanu Patil <shantanupatil1899@gmail.com>
2019-09-30 18:30:36 +0530

    Update TrailingZeros.cpp

--- Math/TrailingZeros.cpp
+++ Math/TrailingZeros.cpp
@@ -4,6 +4,12 @@ n = 5
 n! = 120 
 Number of trailing zeros = 1
 So, return 1
+
+
+Trailing 0s in n! = Count of 5s in prime factors of n!
+                  = floor(n/5) + floor(n/25) + floor(n/125) + ....
+              
+the number of 2s in prime factors is always more than or equal to the number of 5s. 
 */
 
 
